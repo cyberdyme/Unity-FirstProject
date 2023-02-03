b12974217a19a4ba1de89afd8c060116dfcaf5dc
cyberdyme <girish.thanki@gmail.com>
2023-02-03 18:31:05 +0000

    Get the ground check corrected and reset player position

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -83,24 +83,40 @@ public class Player : MonoBehaviour
     */
 
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     private bool isJumping;
 
-    private float coyoteTime = 0.2f;
+    public float coyoteTime = 0.2f;
     private float coyoteTimeCounter;
 
-    private float jumpBufferTime = 0.2f;
+    public float jumpBufferTime = 0.2f;
     private float jumpBufferCounter;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+
+    private Vector3 startPosition;
+
+
+    private void Start()
+    {
+        this.startPosition = this.transform.position;
+    }
+
     private void Update()
     {
+        if (Input.GetKeyDown("r"))
+        {
+            // is a struct so values copied back;
+            this.transform.position = this.startPosition;
+            return;
+        }
+
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (IsGrounded())
@@ -130,9 +146,9 @@ public class Player : MonoBehaviour
             StartCoroutine(JumpCooldown());
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && isJumping)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.1f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
 
             coyoteTimeCounter = 0f;
         }
@@ -147,7 +163,9 @@ public class Player : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
+        Debug.DrawLine(groundCheck.position, groundCheck.position + new Vector3(0.15f, 0.15f, 0f));
+        return Physics2D.OverlapBox(groundCheck.position, groundCheck.localScale, 0.0f, groundLayer);
+        //return Physics2D.OverlapCircle(groundCheck.position, 0.15f, groundLayer);
     }
 
     private void Flip()
@@ -164,7 +182,7 @@ public class Player : MonoBehaviour
     private IEnumerator JumpCooldown()
     {
         isJumping = true;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.05f);
         isJumping = false;
     }
 }
